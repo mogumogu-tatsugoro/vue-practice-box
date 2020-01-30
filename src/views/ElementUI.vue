6b6c70e5970b75627eb204b895c6a446ced36cd6
mogumogu-tatsugoro <mogumogu.tatsugoro@gmail.com>
2020-01-30 23:52:50 +0900

    implement offset layout

--- src/views/ElementUI.vue
+++ src/views/ElementUI.vue
@@ -1,5 +1,6 @@
 <template>
-  <el-row :gutter=20>
+<div>
+  <el-row :gutter="20">
     <el-col :span="8">
       <div class="grid-content bg-purple-dark">
         吾輩は猫である。名前はまだない。どこで生れたか頓と見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
@@ -16,6 +17,12 @@
       </div>
     </el-col>
   </el-row>
+
+  <el-row>
+    <el-col :span="9" :offset="2"><div class="grid-content bg-purple">吾輩は猫である。名前はまだない。どこで生れたか頓と見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。</div></el-col>
+    <el-col :span="9" :offset="1"><div class="grid-content bg-purple">吾輩は猫である。名前はまだない。どこで生れたか頓と見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。</div></el-col>
+  </el-row>
+  </div>
 </template>
 <script>
 export default {
