1151636946cae23d98a02a6fe7ace2147fa5b767
mogumogu-tatsugoro <mogumogu.tatsugoro@gmail.com>
2020-01-30 23:43:44 +0900

    implement layout of element-ui

--- src/views/ElementUI.vue
+++ src/views/ElementUI.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <p>Here is Element UI page</p>
-  </div>
+  <el-row>
+    <el-col :span="8">
+      <div class="grid-content bg-purple-dark">
+        吾輩は猫である。名前はまだない。どこで生れたか頓と見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
+      </div>
+    </el-col>
+    <el-col :span="8">
+      <div class="grid-content bg-purple-dark">
+        吾輩は猫である。名前はまだない。どこで生れたか頓と見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
+      </div>
+    </el-col>
+    <el-col :span="8">
+      <div class="grid-content bg-purple-dark">
+        吾輩は猫である。名前はまだない。どこで生れたか頓と見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
+      </div>
+    </el-col>
+  </el-row>
 </template>
 <script>
 export default {
